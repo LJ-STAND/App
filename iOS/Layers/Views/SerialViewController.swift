516571d35073b14f62ca2ac0305ac05f3de2f0dc
Lachlan Grant <lachlangrant@rbvea.co>
2016-10-12 19:35:14 +1000

    Working Serial

--- iOS/Layers/Views/SerialViewController.swift
+++ iOS/Layers/Views/SerialViewController.swift
@@ -9,19 +9,80 @@
 import UIKit
 import MKKit
 import CoreBluetooth
+import CRToast
+import Chameleon
 
 class SerialViewController: UIViewController {
     
     @IBOutlet weak var sendTextField: UITextField!
     @IBOutlet weak var serialOutputTextView: UITextView!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    var peripherals: [(peripheral: CBPeripheral, RSSI: Float)] = []
+    var selectedPeripheral: CBPeripheral?
     
     override func viewDidLoad() {
         serial = BluetoothSerial(delegate: self)
         if !serial.isReady {
-            //TODO: Present a connect view controller
+            connect()
         }
-        
+        serialOutputTextView.text = ""
         serial.writeType = .withoutResponse
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func textViewScrollToBottom() {
+        let range = NSMakeRange(NSString(string: serialOutputTextView.text).length - 1, 1)
+        serialOutputTextView.scrollRangeToVisible(range)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func connect() {
+        MKAsync.main {
+            //Show Status
+            CRToastManager.dismissAllNotifications(true)
+            
+            let options = [
+                kCRToastTextKey: "Scanning for Bluetooth Devices...",
+                kCRToastTextAlignmentKey: NSTextAlignment.center,
+                kCRToastBackgroundColorKey: UIColor.flatBlue()
+                ] as [String : Any]
+            
+            CRToastManager.showNotification(options: options, completionBlock: {})
+        }.background {
+            serial.startScan()
+            sleep(2)
+        }.main {
+            serial.stopScan()
+            
+            if self.peripherals.count > 0 {
+                let alert = UIAlertController(title: "Connect to Device", message: nil, preferredStyle: .alert)
+                
+                for item in self.peripherals {
+                    alert.addAction(UIAlertAction(title: item.peripheral.name, style: UIAlertActionStyle.default, handler: { (action) in
+                        self.selectedPeripheral = item.peripheral
+                        serial.connectToPeripheral(item.peripheral)
+//                        self.connectingAlert = UIAlertController(title: "Connecting...", message: nil, preferredStyle: .alert)
+//                        self.present(self.connectingAlert, animated: true, completion: nil)
+                    }))
+                }
+                
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                CRToastManager.dismissAllNotifications(false)
+                self.connect()
+            }
+        }
     }
     
     @IBAction func sendAction(_ sender: Any) {
@@ -36,8 +97,52 @@ class SerialViewController: UIViewController {
 
 extension SerialViewController: BluetoothSerialDelegate {
     
+    func serialDidDiscoverPeripheral(_ peripheral: CBPeripheral, RSSI: NSNumber?) {
+        // check whether it is a duplicate
+        for exisiting in peripherals {
+            if exisiting.peripheral.identifier == peripheral.identifier { return }
+        }
+        
+        // add to the array, next sort & reload
+        let theRSSI = RSSI?.floatValue ?? 0.0
+        peripherals.append(peripheral: peripheral, RSSI: theRSSI)
+        peripherals.sort { $0.RSSI < $1.RSSI }
+        
+        log.debug("Found Device")
+    }
+    
+    func serialDidConnect(_ peripheral: CBPeripheral) {
+//        connectingAlert.removeFromParentViewController()
+        let options = [
+            kCRToastTextKey: "Connected to \(peripheral.name!)!",
+            kCRToastTextAlignmentKey: NSTextAlignment.center,
+            kCRToastBackgroundColorKey: UIColor.flatGreen()
+            ] as [String : Any]
+        
+        CRToastManager.showNotification(options: options, completionBlock: {})
+    }
+    
+    func serialDidFailToConnect(_ peripheral: CBPeripheral, error: NSError?) {
+        let options = [
+            kCRToastTextKey: "Connection Failed. \(error?.localizedDescription)",
+            kCRToastTextAlignmentKey: NSTextAlignment.center,
+            kCRToastBackgroundColorKey: UIColor.flatRed()
+            ] as [String : Any]
+        
+        CRToastManager.showNotification(options: options, completionBlock: {})
+        sleep(1)
+//        connectingAlert.removeFromParentViewController()
+        connect()
+    }
+    
     func serialDidReceiveString(_ message: String) {
         //TODO: Append to TextView
+//        log.debug(message)
+        var text = serialOutputTextView.text!
+        text += message
+        serialOutputTextView.text = text
+        self.textViewScrollToBottom()
+        log.debug("Recieved String")
     }
     
     func serialDidChangeState() {
