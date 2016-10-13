598bb23bfbed03cc5b12689859a24fffad82b68c
Lachlan Grant <lachlangrant@rbvea.co>
2016-10-14 09:16:02 +1000

    Combined all subviews into their containing view controller

--- iOS/LJSTAND/Views/CompassViewController.swift
+++ iOS/LJSTAND/Views/CompassViewController.swift
@@ -35,3 +35,38 @@ class CompassViewController: UIViewController {
         angleLabel.text = "Angle: \(ang)"
     }
 }
+
+
+class CompassView: UIView {
+    var needle: UIView = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.lightGray
+        self.layer.cornerRadius = frame.width/2
+        self.clipsToBounds = true
+        
+        self.needle = UIView(frame: CGRect(x: self.frame.width / 4, y: self.frame.height / 2, width: self.frame.width/2, height: 10))
+        self.needle.backgroundColor = .green
+        self.needle.layer.cornerRadius = 5
+        self.needle.clipsToBounds = true
+        
+        self.needle.layer.anchorPoint = CGPoint(x: 0, y: 0.5)
+        //        self.needle.frame.origin = self.center
+        self.addSubview(needle)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func rotate(angle:Double) {
+        let rad = degToRad(angle: angle)
+        self.needle.transform = CGAffineTransform(rotationAngle: CGFloat(rad))
+        //        self.needle.center = self.center
+    }
+    
+    func degToRad(angle: Double) -> Double {
+        return (angle - 90) * M_PI/180
+    }
+}
--- iOS/LJSTAND/Views/TSOPViewController.swift
+++ iOS/LJSTAND/Views/TSOPViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreBluetooth
 import MKKit
-
+import QuartzCore
 
 class TSOPViewController: UIViewController {
     
@@ -39,3 +39,86 @@ class TSOPViewController: UIViewController {
     }
 
 }
+
+class circularView: UIView {
+    var tsops: [tsop] = []
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.lightGray
+        self.layer.cornerRadius = frame.width/2
+        self.clipsToBounds = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func drawTSOPS(numberOfTSOPS: Int) {
+        
+        for tsp in tsops {
+            tsp.removeFromSuperview()
+            
+        }
+        
+        tsops = []
+        
+        let interval = Double(360/numberOfTSOPS)
+        let hypt = Double(self.frame.width - 210)
+        
+        for i in 1...numberOfTSOPS {
+            
+            //Maths stuffs
+            let angle = (interval * Double(i)) - 105
+            let angleRad = (angle * M_PI / 180.0)
+            
+            let hei = (hypt * sin(angleRad)) + Double(self.frame.height / 2) - 15
+            let wid = (hypt * cos(angleRad)) + Double(self.frame.width / 2) - 15
+            
+            
+            let tempTSOP = tsop(frame: CGRect(x: wid, y: hei, width: 30, height: 30))
+            tempTSOP.setTSOP()
+            self.addSubview(tempTSOP)
+            
+            tsops.append(tempTSOP)
+        }
+    }
+    
+    func setCurrent(current: Int) {
+        
+        for tSP in tsops {
+            tSP.current = false
+            tSP.setTSOP()
+        }
+        
+        let curr = tsops[current]
+        
+        curr.current = true
+        curr.setTSOP()
+    }
+}
+
+class tsop: UIView {
+    var current = false
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .green
+        self.layer.cornerRadius = frame.width/2
+        self.clipsToBounds = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError()
+    }
+    
+    func setTSOP() {
+        if (current) {
+            self.backgroundColor = .green
+        } else {
+            self.backgroundColor = .black
+        }
+    }
+    
+}
+
