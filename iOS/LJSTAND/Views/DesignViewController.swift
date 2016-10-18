b6aad0fd26bfae37682a20ae84a64d04b8848fe5
Lachlan Grant <lachlangrant@rbvea.co>
2016-10-19 07:09:42 +1000

    Design View? Thoughts?

---
+++ iOS/LJSTAND/Views/DesignViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DesignViewController.swift
+//  LJ STAND
+//
+//  Created by Lachlan Grant on 19/10/16.
+//  Copyright © 2016 Lachlan Grant. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import Chameleon
+import MKKit
+
+class DesignViewController: UIViewController {
+    var sceneView: SCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sceneView = SCNView(frame: self.view.frame)
+        sceneView.scene = try! SCNScene(url: URL(fileURLWithPath: Bundle.main.path(forResource: "Robot", ofType: "obj")!), options: nil)
+        sceneView.backgroundColor = UIColor.flatWhite()
+        
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.allowsCameraControl = true
+        sceneView.preferredFramesPerSecond = 60
+        sceneView.antialiasingMode = SCNAntialiasingMode.multisampling2X
+        sceneView.sizeToFit()
+        self.view.addSubview(sceneView)
+    }
+}
