5f2ba24a98f28e68133827559558bfd2ff77be67
Lachlan Grant <lachlangrant@rbvea.co>
2017-01-30 17:07:36 +1000

    Updated Frameworks and added scripts

--- iOS/LJSTAND/Controllers/AppDelegate.swift
+++ iOS/LJSTAND/Controllers/AppDelegate.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 import MKKit
+import MKUtilityKit
 import Chameleon
 
-let log = MKLogController().log
+let log = MKULog.shared
 let parts = PartParser()
 
 let ljStandGreen = UIColor.flatGreenColorDark()
@@ -28,29 +29,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func checkForUpdate() {
-        if MKReachability().connectedToNetwork() {
-            do {
-                let urlStr = "https://lj-stand.github.io/Apps/config.json"
-                let url = URL(string: urlStr)
-                
-                let data = try Data(contentsOf: url!)
-                let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
-                
-                let releasedVer = json["currentRelease"] as! Int
-                
-                let thisBuild = Int(MKAppSettingsController().build.components(separatedBy: "-")[0])!
-                
-                if releasedVer > thisBuild {
-                    log.info("Update")
-                    let url = URL(string: "itms-services://?action=download-manifest&url=https://lj-stand.github.io/Apps/dist/manifest.plist")!
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            } catch {
-                log.info("Unable to retrieve JSON data from server")
-            }
-        } else {
-            log.info("No Internet Connection or Debug")
-        }
+//        if MKReachability().connectedToNetwork() {
+//            do {
+//                let urlStr = "https://lj-stand.github.io/Apps/config.json"
+//                let url = URL(string: urlStr)
+//                
+//                let data = try Data(contentsOf: url!)
+//                let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+//                
+//                let releasedVer = json["currentRelease"] as! Int
+//                
+//                let thisBuild = Int(MKAppSettingsController().build.components(separatedBy: "-")[0])!
+//                
+//                if releasedVer > thisBuild {
+//                    log.info("Update")
+//                    let url = URL(string: "itms-services://?action=download-manifest&url=https://lj-stand.github.io/Apps/dist/manifest.plist")!
+//                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+//                }
+//            } catch {
+//                log.info("Unable to retrieve JSON data from server")
+//            }
+//        } else {
+//            log.info("No Internet Connection or Debug")
+//        }
     }
 }
 
---
+++ iOS/MKKit-Dist/iOS/MKKit.framework/Headers/MKKit.h
@@ -0,0 +1,19 @@
+//
+//  MKKit.h
+//  MKKit
+//
+//  Created by Lachlan Grant on 4/10/16.
+//  Copyright © 2016 Lachlan Grant. All rights reserved.
+//
+
+//#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+//! Project version number for MKKit.
+FOUNDATION_EXPORT double MKKitVersionNumber;
+
+//! Project version string for MKKit.
+FOUNDATION_EXPORT const unsigned char MKKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MKKit/PublicHeader.h>
+
