dde65a55b484cdb934fae2197af5f4a2d98009a9
Lachlan Grant <lachlangrant@rbvea.co>
2017-02-27 10:00:09 +1000

    Updated Carthage

---
+++ iOS/LJSTAND/Models/MKKitExt.swift
@@ -0,0 +1,18 @@
+//
+//  MKKitExt.swift
+//  LJ STAND
+//
+//  Created by Lachlan Grant on 27/2/17.
+//  Copyright © 2017 Lachlan Grant. All rights reserved.
+//
+
+import Foundation
+import MKKit
+
+extension MKAppGroups {
+    static let LJSTAND = "group.com.lachlangrant.LJSTAND"
+}
+
+extension MKBundleID {
+    static let LJSTAND = "com.lachlangrant.LJSTAND"
+}
