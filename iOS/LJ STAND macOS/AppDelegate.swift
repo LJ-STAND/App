b6771a71dc00e035bcb56e82525983414b3c2c1f
Lachlan Grant <lachlangrant@myMBPR.local>
2017-03-20 09:59:22 +1000

    Semi Working LightSensors

--- iOS/LJ STAND macOS/AppDelegate.swift
+++ iOS/LJ STAND macOS/AppDelegate.swift
@@ -7,20 +7,69 @@
 //
 
 import Cocoa
+import MKKit
+import MKUtilityKit
+import CoreBluetooth
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+		BluetoothController.shared.messageDelegate = self
+        BluetoothController.shared.compassDelegate = self
+        BluetoothController.shared.serialDelegate = self
+        BluetoothController.shared.tsopDelegate = self
+        MKULog.shared.logDetails()
 	}
-
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
-	}
-
-
 }
 
+extension AppDelegate: BluetoothMessageDelegate {
+    func showError(_ message: String) {
+        commonAlert(message)
+    }
+    
+    func showInformation(_ message: String) {
+        commonAlert(message)
+    }
+    
+    func dismissNotifications() {
+        //TODO:
+    }
+    
+    func foundDevices(_ peripherals: [CBPeripheral]) {
+        if peripherals.count == 1 {
+            BluetoothController.shared.connectTo(peripheral: peripherals.first!)
+        }
+    }
+    
+    func commonAlert(_ message: String) {
+        let alert = NSAlert()
+        
+        alert.alertStyle = NSAlertStyle.warning
+        alert.messageText = message
+        alert.addButton(withTitle: "Close")
+        alert.delegate = self
+        alert.runModal()
+    }
+}
+
+extension AppDelegate: BluetoothControllerTSOPDelegate, BluetoothControllerSerialDelegate,BluetoothControllerCompassDelegate {
+    func hasNewHeading(_ angle: Double) {
+        commonAlert("Angle: \(angle)")
+    }
+    
+    func hasNewOutput(_ serial: String) {
+        commonAlert(serial)
+    }
+    
+    func hasNewActiveTSOP(_ tsopNum: Int) {
+        commonAlert("TSOP: \(tsopNum)")
+    }
+}
+
+extension AppDelegate: NSAlertDelegate {
+    func alertShowHelp(_ alert: NSAlert) -> Bool {
+        print([alert])
+        return true
+    }
+}
--- iOS/LJ STAND macOS/ViewController.swift
+++ iOS/LJ STAND macOS/ViewController.swift
@@ -8,12 +8,14 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class MacOSViewController: NSViewController, BluetoothControllerLightSensorDelegate {
+	@IBOutlet weak var lightSensor: lightSensorView!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		// Do any additional setup after loading the view.
+        BluetoothController.shared.lightSensDelegate = self
 	}
 
 	override var representedObject: Any? {
@@ -22,6 +24,96 @@ class ViewController: NSViewController {
 		}
 	}
 
-
+    func updatedCurrentLightSensors(_ sensors: [Int]) {
+        lightSensor.clearValues()
+        
+        for item in sensors {
+            lightSensor.setValues(item)
+        }
+    }
 }
+
+
+@IBDesignable class lightSensorView: NSView {
+    var lights: [Bool] = [Bool]()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        for _ in 1...24 {
+            lights.append(false)
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let path = NSBezierPath(rect: rect)
+        NSColor.white.setFill()
+        
+        path.fill()
+        
+        let numberOfLights = 24
+        
+        let radOfLight = Double(self.frame.width / 24)
+        let offset = radOfLight / 2.0
+        
+        let interval = Double(360 / numberOfLights)
+        let hypt = Double(self.frame.width/2) - radOfLight
+        
+        for i in 0..<numberOfLights {
+            let angle = 360 - ((interval * Double(i)) + 90)
+            let angleRad = degToRad(angle)
+            
+            let xVal = (Double(self.frame.width/2) + (hypt * sin(angleRad)) - offset)
+            let yVal = (Double(self.frame.height/2) + (hypt * cos(angleRad)) - offset)
+            
+            let path = NSBezierPath(ovalIn: CGRect(x: xVal, y: yVal, width: radOfLight, height: radOfLight))
+            NSColor.black.setFill()
+            NSColor.black.setStroke()
+            
+            if lights[i] {
+                path.fill()
+            } else {
+                path.stroke()
+            }
+        }
+        
+        if !BluetoothController.shared.connected {
+            let ovalRect = rect.insetBy(dx: 0.9 * (rect.size.width / 2), dy: 0.9 * (rect.size.height / 2))
+            let ovalPath = NSBezierPath(ovalIn: ovalRect)
+            ovalPath.move(to: CGPoint(x: ovalRect.midX + (ovalRect.width / 2) * CGFloat(cos(3*(Double.pi / 4))), y: ovalRect.midY + (ovalRect.width / 2) * CGFloat(sin(3*(Double.pi / 4)))))
+            //            ovalPath.addLine(to: CGPoint(x: ovalRect.midX + (ovalRect.width / 2) * CGFloat(cos(-(Double.pi / 4))), y: ovalRect.midY + (ovalRect.width / 2) * CGFloat(sin(-(Double.pi / 4)))))
+            
+            NSColor.red.setStroke()
+            ovalPath.lineWidth = 3
+            ovalPath.stroke()
+        }
+    }
+    
+    func setValues(_ sensorNumber: Int) {
+        lights[sensorNumber] = true
+        
+        setNeedsDisplay(bounds)
+    }
+    
+    func clearValues() {
+        for i in 0..<lights.count {
+            lights[i] = false
+        }
+        
+        setNeedsDisplay(bounds)
+    }
+    
+    func degToRad(_ angle: Double) -> Double {
+        return (angle - 90) * Double.pi/180
+    }
+}
+
 
